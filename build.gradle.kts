36bec733805cd11a7091d108305f410ac4cd36a3
TheGatesScratch <mtkarels@outlook.com>
2023-05-26 16:35:47 +0200

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("java")
+}
+
+group = "io.github.thegatesdev"
+version = "0.1"
+description = "Limit player online time"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+}
+
+tasks{
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+        val props = mapOf(
+            "name" to project.name,
+            "version" to project.version,
+            "description" to project.description,
+            "apiVersion" to "1.19"
+        )
+        inputs.properties(props)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+}
---
+++ src/main/java/io/github/thegatesdev/playertimer/PlayerTimer.java
@@ -0,0 +1,8 @@
+package io.github.thegatesdev.playertimer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlayerTimer extends JavaPlugin {
+
+
+}
