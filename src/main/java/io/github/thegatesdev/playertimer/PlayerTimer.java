556404a5660cd288d470a9f23b1b6a4018031860
TheGatesScratch <mtkarels@outlook.com>
2023-05-29 20:04:09 +0200

    Make settings configurable.

--- src/main/java/io/github/thegatesdev/playertimer/PlayerTimer.java
+++ src/main/java/io/github/thegatesdev/playertimer/PlayerTimer.java
@@ -1,22 +1,53 @@
 package io.github.thegatesdev.playertimer;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.time.Duration;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Objects;
 
 public class PlayerTimer extends JavaPlugin {
-    private final PlayerTimerHandler playerTimerHandler = new PlayerTimerHandler(this,
-            new PlayerTimerHandler.Settings()
-                    .setMaxOnlineTime(Duration.ofSeconds(20))
-    );
+    private PlayerTimerHandler playerTimerHandler;
+
+    @Override
+    public void onLoad() {
+        var config = getConfig();
+
+        var settings = new PlayerTimerHandler.Settings();
+        if (config.isString("time-zone"))
+            settings.zoneId(ZoneId.of(Objects.requireNonNull(config.getString("timezone"))));
+        if (config.isConfigurationSection("time-limit")) {
+            var section = Objects.requireNonNull(config.getConfigurationSection("timelimit"));
+            settings.setMaxOnlineTime(
+                    Duration.ofHours(section.getInt("hours", 0))
+                            .plusMinutes(section.getInt("minutes", 0))
+                            .plusSeconds(section.getInt("seconds", 0))
+            );
+        }
+        if (config.isConfigurationSection("reset-time")) {
+            var section = Objects.requireNonNull(config.getConfigurationSection("reset-time"));
+            settings.resetTime(LocalTime.of(
+                    section.getInt("hours", 0),
+                    section.getInt("minutes", 0),
+                    section.getInt("seconds", 0)
+            ));
+        }
+
+        playerTimerHandler = new PlayerTimerHandler(this, settings);
+
+        getLogger().info("SETTINGS: Timezone='%s' Timelimit='%s' ResetTime='%s'"
+                .formatted(
+                        settings.zoneId.getId(),
+                        settings.maxOnlineTime.toHoursPart() + "h " +
+                                settings.maxOnlineTime.toMinutesPart() + "m " +
+                                settings.maxOnlineTime.toSecondsPart() + "s",
+                        settings.resetTime
+                ));
+    }
 
     @Override
     public void onEnable() {
-        var s = playerTimerHandler.settings();
-        Bukkit.getLogger().info("Time limit: " + s.maxOnlineTime.toSeconds() + " seconds or " + s.maxOnlineTime.toMinutes() + " minutes");
-        Bukkit.getLogger().info("Resets at: " + s.resetTime.toString());
-        Bukkit.getLogger().info("Timezone: " + s.zoneId.getId());
         playerTimerHandler.init();
     }
 }
