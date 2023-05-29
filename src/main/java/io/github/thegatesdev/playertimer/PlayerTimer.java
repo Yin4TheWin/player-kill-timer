d6f28bccce27feaf114f407ad40674524c9f48ab
TheGatesScratch <mtkarels@outlook.com>
2023-05-29 13:58:21 +0200

    Fix changes. Display config nicely.

--- src/main/java/io/github/thegatesdev/playertimer/PlayerTimer.java
+++ src/main/java/io/github/thegatesdev/playertimer/PlayerTimer.java
@@ -3,16 +3,18 @@ package io.github.thegatesdev.playertimer;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.time.Duration;
+
 public class PlayerTimer extends JavaPlugin {
     private final PlayerTimerHandler playerTimerHandler = new PlayerTimerHandler(this,
             new PlayerTimerHandler.Settings()
-                    .maxMinutesOnline(1)
+                    .setMaxOnlineTime(Duration.ofSeconds(20))
     );
 
     @Override
     public void onEnable() {
         var s = playerTimerHandler.settings();
-        Bukkit.getLogger().info("Time limit: " + s.maxTicksOnline / 20 / 60 + " minutes");
+        Bukkit.getLogger().info("Time limit: " + s.maxOnlineTime.toSeconds() + " seconds or " + s.maxOnlineTime.toMinutes() + " minutes");
         Bukkit.getLogger().info("Resets at: " + s.resetTime.toString());
         Bukkit.getLogger().info("Timezone: " + s.zoneId.getId());
         playerTimerHandler.init();
